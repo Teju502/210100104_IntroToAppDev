2cdf4951bbe6a4c5758fb2e983b78043fe8d73f7
Teju502 <110400468+Teju502@users.noreply.github.com>
2023-07-12 23:25:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,75 +1,78 @@
-import 'package:flutter/material.dart'; 
-  
- void main() { 
-   runApp(CounterApp()); 
- } 
-  
- class CounterApp extends StatefulWidget { 
-   @override 
-   CounterAppState createState() => CounterAppState(); 
- } 
-  
- class CounterAppState extends State<CounterApp> { 
-   int counter = 0; 
-  
-   void _incrementCounter() { 
-     setState(() { 
-       counter++; 
-     }); 
-   } 
-  
-   void _decrementCounter() { 
-     setState(() { 
-       counter--; 
-     }); 
-   } 
-  
-   @override 
-   Widget build(BuildContext context) { 
-     return MaterialApp( 
-       title: 'Counter App', 
-       home: Scaffold( 
-         appBar: AppBar( 
-           title: Text('Counter App'), 
-         ), 
-         backgroundColor: Colors.yellow, // Change the background color here 
-         body: Center( 
-           child: Column( 
-             mainAxisAlignment: MainAxisAlignment.center, 
-             children: <Widget>[ 
-               Text( 
-                 'Counter Value:', 
-                 style: TextStyle(fontSize: 24), 
-               ), 
-               Text( 
-                 '$counter', 
-                 style: TextStyle(fontSize: 48), 
-               ), 
-               SizedBox(height: 60), 
-               Row( 
-                 mainAxisAlignment: MainAxisAlignment.center, 
-                 children: <Widget>[ 
-                   ElevatedButton( 
-                     onPressed: _incrementCounter, 
-                     child: Text('INC'), 
-                     style: ElevatedButton.styleFrom( 
-                       primary: Colors.red, 
-                     ), 
-                   ), 
-                   SizedBox(width: 60), 
-                   ElevatedButton( 
-                     onPressed: _decrementCounter, 
-                     child: Text('DEC'), 
-                     style: ElevatedButton.styleFrom( 
-                       primary: Colors.green, 
-                     ), 
-                   ), 
-                 ], 
-               ), 
-             ], 
-           ), 
-         ), 
-       ), 
-     ); 
-   } 
- }
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CounterApp());
+}
+
+class CounterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CounterScreen(),
+    );
+  }
+}
+
+class CounterScreen extends StatefulWidget {
+  @override
+  _CounterScreenState createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int counter = 0;
+
+  void incrementCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  void decrementCounter() {
+    setState(() {
+      counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Counter',
+              style: TextStyle(fontSize: 24),
+            ),
+            Text(
+              '$counter',
+              style: TextStyle(fontSize: 48),
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: incrementCounter,
+                  child: Text('+'),
+                ),
+                SizedBox(width: 16),
+                ElevatedButton(
+                  onPressed: decrementCounter,
+                  child: Text('-'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
